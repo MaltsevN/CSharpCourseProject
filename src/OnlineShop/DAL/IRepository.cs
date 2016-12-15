faaf6b08792aa0157b02b4192cd4c4844336c439
Ivan Pugach <ivanpugachit@mail.ru>
2016-12-15 13:37:59 +0200

    Task 2

---
+++ src/OnlineShop/DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    interface IRepository<T> where T : class
+    {
+        void Create(T item);
+        void Delete(int id);
+        T GetItem(int id);
+        IEnumerable<T> GetItemsList();
+        void Save();
+        void Update(T item);
+    }
+}
