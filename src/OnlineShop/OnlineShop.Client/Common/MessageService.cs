b75e402b73fb03693ba5d269bc93dc75072c2bc7
PavelPisotskiy <pisockiypavel@gmail.com>
2016-12-15 22:05:54 +0200

    Task 22 is done

---
+++ src/OnlineShop/OnlineShop.Client/Common/MessageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OnlineShop.Client.Common
+{
+    class MessageService : IMessageService
+    {
+        public MessageBoxResult ShowMessage(string message, string header, MessageBoxButton button, MessageBoxImage icon)
+        {
+            return MessageBox.Show(message, header, button, icon);
+        }
+    }
+}
