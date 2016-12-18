cb3c6b5163d232f46b4a34072db99f594e138346
Ivan Pugach <ivanpugachit@mail.ru>
2016-12-18 12:56:30 +0200

    ydaleno svoistvo user v order pereimenoval context i dobavlen konstryctor po ymolchaniiy v usere dobavlen list orders

---
+++ src/OnlineShop/DAL/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace DAL
+{
+    public class AppContext : DbContext
+    {
+        public AppContext() : base("DefaultConnection")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
