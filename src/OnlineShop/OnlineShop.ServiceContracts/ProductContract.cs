20bd4fad4c863ad6f5bbf6a26366c50ed0e46177
Nikita Maltsev <nikitamalcev1@gmail.com>
2016-12-20 19:12:48 +0200

    Task 67 is done

---
+++ src/OnlineShop/OnlineShop.ServiceContracts/ProductContract.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using DAL;
+using DomainModel;
+using System.ServiceModel.Web;
+using System.Threading.Tasks;
+
+namespace OnlineShop.ServiceContracts
+{
+    class ProductContract: IProductContract
+    {
+        IRepository<Product> product;
+
+        public ProductContract(IRepository<Product> product)
+        {
+            this.product = product;
+        }
+
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        public void Create(Product item)
+        {
+            product.Create(item);
+            product.Save();
+        }
+
+        [WebInvoke(Method = "DELETE", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        public void Delete(int id)
+        {
+            product.Delete(id);
+            product.Save();
+        }
+
+        [WebGet(ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, UriTemplate = "GetProduct/{id}")]
+        public Product GetItem(string id)
+        {
+            return product.GetItem(Convert.ToInt32(id));
+        }
+
+        [WebGet(ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, UriTemplate = "GetAllProducts")]
+        public IEnumerable<Product> GetItemsList()
+        {
+            return product.GetItemsList();
+        }
+
+        [WebInvoke(Method = "PUT", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        public void Update(Product item)
+        {
+            product.Update(item);
+            product.Save();
+        }
+    }
+}
