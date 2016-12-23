88526b6dd138dff8dea1b97d78ede9195d01e77e
PavelPisotskiy <pisockiypavel@gmail.com>
2016-12-23 09:24:59 +0200

    Task 92 is done

--- src/OnlineShop/OnlineShop.Client/Services/ProductService.cs
+++ src/OnlineShop/OnlineShop.Client/Services/ProductService.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DomainModel;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace OnlineShop.Client.Services
 {
     class ProductService : IProductService
     {
+        private readonly HttpClient client;
+
+        public ProductService()
+        {
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects
+            };
+            client = new HttpClient();
+        }
+
         public Product Create(Product product)
         {
             throw new NotImplementedException();
@@ -26,7 +39,16 @@ namespace OnlineShop.Client.Services
 
         public IEnumerable<Product> GetProducts()
         {
-            throw new NotImplementedException();
+            IEnumerable<Product> products = new Product[0];
+            string requestUri = Properties.Resources.UrlToServer + "Product/GetAllProducts";
+            var responceMessage = client.GetAsync(requestUri).Result;
+            if (responceMessage.IsSuccessStatusCode)
+            {
+                string jsonResult = responceMessage.Content.ReadAsStringAsync().Result;
+                products = JsonConvert.DeserializeObject<IEnumerable<Product>>(jsonResult);
+            }
+
+            return products;
         }
 
         public void Update(Product product)
