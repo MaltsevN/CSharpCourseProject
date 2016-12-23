2471e3a849be4f36894b08d745a707e7e782e502
PavelPisotskiy <pisockiypavel@gmail.com>
2016-12-23 08:56:48 +0200

    Task 89 is done

--- src/OnlineShop/DomainModel/OrderItem.cs
+++ src/OnlineShop/DomainModel/OrderItem.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DomainModel
 {
@@ -18,5 +19,12 @@ namespace DomainModel
 
         [DataMember]
         public Product Product { get; set; }
+
+        [DataMember]
+        public int OrderId { get; set; }
+
+        [DataMember]
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
     }
 }
